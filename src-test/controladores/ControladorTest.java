ff8098083e0af72a371147e16c5fdaa265a13017
Jenkins <jenkins@jenkins.com>
2015-07-24 17:33:33 -0300

    Agregar test

---
+++ src-test/controladores/ControladorTest.java
@@ -0,0 +1,48 @@
+package controladores;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ControladorTest {
+
+    @Test
+    public void testAltaPersonaSistema() throws Exception {
+
+    }
+
+    @Test
+    public void testAltaVehiculoSistema() throws Exception {
+
+    }
+
+    @Test
+    public void testBuscarPersona() throws Exception {
+
+    }
+
+    @Test
+    public void testListarTitulares() throws Exception {
+
+    }
+
+    @Test
+    public void testTransferirTitular() throws Exception {
+
+    }
+
+    @Test
+    public void testAsignarRolPersona() throws Exception {
+
+    }
+
+    @Test
+    public void testSacarRolPersona() throws Exception {
+
+    }
+
+    @Test
+    public void testBajaVehiculo() throws Exception {
+
+    }
+}
