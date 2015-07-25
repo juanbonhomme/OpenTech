80ca8150a94183859626b7eba5f01b4f23bb8823
Jenkins <jenkins@jenkins.com>
2015-07-25 16:41:47 -0300

    Agregar test

--- src-test/controladores/ControladorTest.java
+++ src-test/controladores/ControladorTest.java
@@ -1,5 +1,7 @@
 package controladores;
 
+import clases.Persona;
+import enumerados.TipoRol;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -12,5 +14,45 @@ public class ControladorTest {
     @Test
     public void testGetInstance() throws Exception {
         new Controlador();
+    }
+
+    @Test
+    public void testAltaPersonaSistema() throws Exception {
+        new Controlador().altaPersonaSistema(new Persona("","","","","", TipoRol.Administrador,""));
+    }
+
+    @Test
+    public void testAltaVehiculoSistema() throws Exception {
+        new Controlador();
+    }
+
+    @Test
+    public void testBuscarPersona() throws Exception {
+        new Controlador();
+    }
+
+    @Test
+    public void testListarTitulares() throws Exception {
+
+    }
+
+    @Test
+    public void testTransferirTitular() throws Exception {
+
+    }
+
+    @Test
+    public void testAsignarRolPersona() throws Exception {
+
+    }
+
+    @Test
+    public void testSacarRolPersona() throws Exception {
+
+    }
+
+    @Test
+    public void testBajaVehiculo() throws Exception {
+
     }
 }
